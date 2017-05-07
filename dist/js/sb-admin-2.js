baf8f5862126d3d8602de9302ce5e51ee48bf114
Aditya Kiran Pentyala <aditya.pentyala@outlook.com>
2017-05-07 10:51:26 -0700

    Final version 1.0

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -10,6 +10,7 @@ var test;
 
 $(document).ready(function(){
 //    $("#cand1").hide();
+    $("#loadData1").hide();
     $("#cand2").hide();
     $("#cand3").hide();
     $("#cand4").hide();
@@ -206,14 +207,14 @@ app.controller("myCtrl", function($scope, $http) {
 
 app.controller('load_data', function($scope, $http){
     $scope.senddata = function(){
-        console.log("I asmf idiot");
+//        console.log("I asmf idiot");
         var startDate = $("#sdate1").val();
-        console.log(startDate);
+//        console.log(startDate);
         startDate = moment(startDate, "D MMM, YYYY").format();
         var endDate = $("#edate1").val();
-        alert(startDate);
+//        alert(startDate);
         endDate = moment(endDate, "D MMM, YYYY").format();
-        alert(startDate);
+//        alert(startDate);
         $scope.startDate = startDate;
         $scope.endDate = endDate;
         
@@ -221,7 +222,7 @@ app.controller('load_data', function($scope, $http){
         
             Waiting for the API.
         */
-        $http.post('test.json'
+        $http.get("test.json"
 //                  ,
 //                   { 'timeMin' : startDate,
 //                     'timeMax' : endDate,
@@ -233,67 +234,95 @@ app.controller('load_data', function($scope, $http){
             for(i=0;i<$scope.busySlots.length;i++)
             {
                 /*2017-05-05T11:00:00-07:00*/
-                console.log($scope.busySlots[i].start);
+//                console.log($scope.busySlots[i].start);
                 test = $scope.busySlots[i].start;
-                console.log(test);
+//                console.log(test);
                 $scope.busySlots[i].start = moment($scope.busySlots[i].start);
                 $scope.busySlots[i].end = moment($scope.busySlots[i].end);
+//                console.log($scope.busySlots[i]);
+                
             }
             
-            $scope.availableSlots = [{ date: '25/01/17', times: [ '12:00', '13:00' ] }, { date: '26/01/17', times: [ '12:00', '13:00' ] }];
-//            test = $scope.startDate;
-//            alert($scope.startDate);
-//            smallStart = moment($scope.startDate);
-//            smallEnd = moment(moment($scope.startDate).add(15, 'm')).format();
+//            $scope.availableSlots = [{ date: '25/01/17', times: [ '12:00', '13:00' ] }, { date: '26/01/17', times: [ '12:00', '13:00' ] }];
+            $scope.availableSlots = [];
+            smallStart = moment($scope.startDate).format();
+            smallEnd = moment(moment($scope.startDate).add(15, 'm')).format();
+//            console.log(smallStart);
 //            console.log(smallEnd);
-//            while(smallEnd<=$scope.endDate)
-//            {
-//                            console.log("kill me again......."+smallEnd);
-//
-//                flag = false;
-//                for(i=0;i<$scope.busySlots.length;i++)
-//                {
-//                    2017-05-05T11:00:00-07:00
-//                    if(($scope.busySlots[i].startDate<smallEnd && $scope.busySlots[i].endDate>smallEnd) || ($scope.busySlots[i].startDate>smallStart && $scope.busySlots[i].endDate<smallStart) || (($scope.busySlots[i].startDate>smallStart && $scope.busySlots[i].endDate<smallEnd)) )
-//                        flag = true;
-//                }
-//                if(!flag)
-//                {
-//                    test = $scope.availableSlots;
-//                    $scope.availableSlots.push({date: smallStart.format("yyyy-m-d"), times : smallStart.format("H:m")});
-//                    console.log("sdfjhdjdkvfdslkjfvdsk");
-//                    console.log($scope.availableSlots);
-//                }
-//                smallStart = smallEnd
-//                test = smallEnd;
-//                smallEnd = moment(moment(smallEnd).add(15, 'm')).format();
-//            
-//            }
-//            
-////            test = $scope.availableSlots;
+//            console.log($scope.busySlots);
+//            console.log(smallEnd);
+            while(smallEnd<=$scope.endDate)
+            {
+                flag = false;
+                for(i=0;i<$scope.busySlots.length;i++)
+                {
+                   
+                    smallStart = moment(smallStart);
+                    smallEnd = moment(smallEnd);
+                    if(($scope.busySlots[i].start<smallEnd && $scope.busySlots[i].end>smallEnd) || ($scope.busySlots[i].start>smallStart && $scope.busySlots[i].end<smallStart) || (($scope.busySlots[i].start>smallStart && $scope.busySlots[i].end<smallEnd)) )
+                        {
+                            flag = true;
+                            break;
+                        }
+                }
+                if(!flag)
+                {
+                    $scope.availableSlots.push({date: smallStart.format("DD/MM/YY"), times : smallStart.format("HH:mm")});
+                }
+                smallStart = smallEnd
+                smallEnd = moment(moment(smallEnd).add(15, 'm')).format();
+            }
 //            console.log($scope.availableSlots);
-//            
-            
             /*
                 Data is the JSON file, parse it.
             */
+            
+            
+            /*parsing the same date multiple times*/
+            
+            $scope.finaldisplay = [];
+            for(i=0;i<$scope.availableSlots.length;i++)
+            {
+                flg = true;
+                for(j=0;j<$scope.finaldisplay.length;j++)
+                {
+                    if($scope.finaldisplay[j].date == $scope.availableSlots[i].date)
+                    {
+                        $scope.finaldisplay[j].times.push($scope.availableSlots[i].times);
+                        flg=false;
+                    }
+                }
+                if(flg)
+                {
+//                    console.log($scope.availableSlots);
+                    date = $scope.availableSlots[i].date;
+                    console.log(date);
+                    times = [];
+                    times.push($scope.availableSlots[i].times);
+                    console.log(times);
+                    $scope.finaldisplay.push({"date":date,"times":times})
+//                    $scope.finaldisplay[j].push($scope.availableSlots.times);
+                }
+            }
+            console.log($scope.finaldisplay);
+            
         });
     }
     
     $scope.removeDate=function(dat)
     {
-        $scope.availableSlots = $scope.availableSlots.filter(function(el) {
+        $scope.finaldisplay = $scope.finaldisplay.filter(function(el) {
             return el.date !== dat;
         });
     }
     
     $scope.removeTime = function(dat, tim)
     {
-        for(i=0; i<$scope.availableSlots.length;i++)
+        for(i=0; i<$scope.finaldisplay.length;i++)
         {
-            if($scope.availableSlots[i].date == dat)
+            if($scope.finaldisplay[i].date == dat)
             {
-                $scope.availableSlots[i].times = $scope.availableSlots[i].times.filter(function(el){
+                $scope.finaldisplay[i].times = $scope.finaldisplay[i].times.filter(function(el){
                    return el !== tim; 
                 });
             }
@@ -301,6 +330,14 @@ app.controller('load_data', function($scope, $http){
     }
     
     $scope.sendSelectedDates = function(){
+        console.log($scope.finaldisplay.length);
+        $("#chose_time").hide();
+        $("#loadData1").show();
+        if($scope.finaldisplay.length == 0)
+        {
+            alert("Cannot send zero records...");
+            return;
+        }
         $http.post('url',{
                       'summary': 'Blah Blah Blah',
                       'location': 'Blah Blah Blah',
